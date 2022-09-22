d84cb2722c78b89f508000787807ebc151740c07
Chatter-Boo <psakhil211@gmail.com>
2022-09-22 06:41:32 +0530

    bottom nav bar 1/2

---
+++ lib/screens/transaction/trans.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScreenTrans extends StatelessWidget {
+  const ScreenTrans({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

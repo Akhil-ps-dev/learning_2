66d4877a5c4a28cde3505ef32c1397b8db46f4e1
Chatter-Boo <psakhil211@gmail.com>
2022-09-23 20:46:50 +0530

    tab bar completed

--- lib/screens/category/cate.dart
+++ lib/screens/category/cate.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
-class ScreenCategory extends StatelessWidget {
+
+class ScreenCategory extends StatefulWidget {
   const ScreenCategory({super.key});
 
   @override
+  State<ScreenCategory> createState() => _ScreenCategoryState();
+}
+
+class _ScreenCategoryState extends State<ScreenCategory>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabcontoller;
+  @override
+  void initState() {
+    _tabcontoller = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('cat'),
+    return Column(
+      children: [
+        TabBar(
+          controller: _tabcontoller,
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.grey,
+          tabs: [
+            Tab(
+              text: 'Expense',
+            ),
+            Tab(
+              text: 'Income',
+            ),
+          ],
+        )
+      ],
     );
   }
 }
--- lib/screens/transaction/trans.dart
+++ lib/screens/transaction/trans.dart
@@ -5,8 +5,27 @@ class ScreenTrans extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('trans'),
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView.separated(
+        itemBuilder: (ctx, index) => Card(
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.black,
+              radius: 50,
+              child: Text('25\ndec'),
+            ),
+            title: Text(' Travel'),
+            subtitle: Text('rs 10000'),
+          ),
+        ),
+        separatorBuilder: ((ctx, index) {
+          return const SizedBox(
+            height: 10,
+          );
+        }),
+        itemCount: 5,
+      ),
     );
   }
 }
